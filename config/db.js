28b8459f70c285b223ae30c019ca89f925dae5f6
Mahady Hasan <marofbd@gmail.com>
2025-01-17 13:26:49 +0100

    Remove config.env from tracking

--- config/db.js
+++ config/db.js
@@ -17,25 +17,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-
-//-----------Backup old file-------------
-// const mongoose = require('mongoose');
-
-// const connectDB = async ()=> {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//        useUnifiedTopology: true
-//     });
-
-//     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-//   } catch (err) {
-//     console.log(`Error: ${err.message}`.red);
-//     process.exit(1);
-//   }
-// }
-
-// module.exports = connectDB;
